2b25735281554c37b3d6ab07f2624a8adcddfcb7
Marlonn Locatelli <marlonn@gmail.com>
2024-10-07 21:19:04 -0300

    Adicionado função de busca.

--- src/containers/TabBar/index.tsx
+++ src/containers/TabBar/index.tsx
@@ -4,12 +4,23 @@ import { BuscarInput, CampoBusca, Tabs, TabsContainer } from './styles'
 import * as enums from '../../utils/enums/contatos'
 import AddButton from '../../components/AddButton'
 import { BootstrapIcon } from '../../styles'
+import { useDispatch, useSelector } from 'react-redux'
+import { RootReducer } from '../../store'
+import { alteraTermo } from '../../store/reducers/filtro'
 
 const TabBar = () => {
+  const dispatch = useDispatch()
+  const { termoBusca } = useSelector((state: RootReducer) => state.filtro)
+
   return (
     <>
       <CampoBusca>
-        <BuscarInput type="text" name="" id="" placeholder="pesquisar" />
+        <BuscarInput
+          type="text"
+          placeholder="pesquisar"
+          value={termoBusca}
+          onChange={(e) => dispatch(alteraTermo(e.target.value))}
+        />
         <BootstrapIcon>&#xF52A;</BootstrapIcon>
       </CampoBusca>
 
